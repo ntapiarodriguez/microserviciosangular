0438870de0c1dcd60888eb66f21614d2d873a973
Natalia Tapia <nataliatapia@MacBook-Pro-de-Natalia-2.local>
2020-07-30 12:22:23 -0400

    113. Creando el layout y navbar utilizando módulos

--- cursos/src/app/components/alumnos/alumnos.component.ts
+++ cursos/src/app/components/alumnos/alumnos.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlumnosComponent implements OnInit {
 
+  titulo = 'Listado de Alumnos';
   constructor() { }
 
   ngOnInit(): void {
