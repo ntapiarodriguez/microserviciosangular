29b8a5b25e060765aeb58763154411ec5d36dc3d
Natalia Tapia <nataliatapia@MacBook-Pro-de-Natalia-2.local>
2020-07-31 16:37:58 -0400

    Creando nuestras clases modelos typescript

--- cursos/src/app/models/asignatura.ts
+++ cursos/src/app/models/asignatura.ts
@@ -1,2 +1,6 @@
 export class Asignatura {
+  id: number;
+  nombre: string;
+  padre: Asignatura;
+  hijos: Asignatura[] = [];
 }
--- cursos/src/app/models/pregunta.ts
+++ cursos/src/app/models/pregunta.ts
@@ -1,2 +1,7 @@
+import { Examen } from "./examen";
+
 export class Pregunta {
+  id: number;
+  texto: string;
+  examen: Examen
 }
